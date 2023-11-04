608b6dfb4abf6e974b5fc06013200461c9da5bb5
Krtz_Dev <florian.krtz@gmail.com>
2023-11-04 22:04:58 +0100

    Setup Ability Lists OnSceneLoaded

--- Assets/Scripts/Inventory/AbilityManager.cs
+++ Assets/Scripts/Inventory/AbilityManager.cs
@@ -1,403 +1,414 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AbilityManager : Singleton<AbilityManager>
 {
-    [SerializeField] private List<TaskAndGiveGenerator.TakeByID> _takeByIDs = new();
-    [SerializeField] private List<TaskAndGiveGenerator.GiveByID> _giveByIDs = new();
+	[SerializeField] private List<TaskAndGiveGenerator.TakeByID> _takeByIDs = new();
+	[SerializeField] private List<TaskAndGiveGenerator.GiveByID> _giveByIDs = new();
 
-    private List<int> _takeIDs = new();
-    private List<int> _giveIDs = new();
+	private List<int> _takeIDs = new();
+	private List<int> _giveIDs = new();
+
+	[SerializeField] private List<bool> _takeTaskBoolList = new();
+	[SerializeField] private List<bool> _giveTaskBoolList = new();
 
-    [SerializeField] private List<bool> _takeTaskBoolList = new();
-    [SerializeField] private List<bool> _giveTaskBoolList = new();
+	protected override void Awake()
+	{
+		base.Awake();
 
-    //private void Start() // Load scene! Do each time a level is started.
-    //{
-    //    ConvertToIDList(_takeByIDs);
-    //    ConvertToIDList(_giveByIDs);
+		SceneLoader.instance.OnSceneLoadCompleted += OnSceneLoaded;
+	}
 
-    //    _takeTaskBoolList = new();
-    //    _giveTaskBoolList = new();
+	private void OnSceneLoaded()
+	{
+		SetupTakeAndGiveLists();
+	}
 
-    //    for(int i = 0; i < _takeByIDs.Count; i++)
-    //    {
-    //        _takeTaskBoolList.Add(false);
-    //    }
+	private void SetupTakeAndGiveLists() // Load scene! Do each time a level is started.
+	{
+		ConvertToIDList(_takeByIDs);
+		ConvertToIDList(_giveByIDs);
 
-    //    for (int i = 0; i < _giveByIDs.Count; i++)
-    //    {
-    //        _giveTaskBoolList.Add(false);
-    //    }
+		_takeTaskBoolList = new();
+		_giveTaskBoolList = new();
 
-    //    for (int i = 0; i < _takeByIDs.Count; i++)
-    //    {
-    //        if (ListContainsIndex(_takeIDs, _takeByIDs[i].id))
-    //            _takeTaskBoolList[_takeByIDs[i].id] = true;
-    //    }
+		for (int i = 0; i < _takeByIDs.Count; i++)
+		{
+			_takeTaskBoolList.Add(false);
+		}
 
-    //    for (int i = 0; i < _giveByIDs.Count; i++)
-    //    {
-    //        if(ListContainsIndex(_giveIDs, _giveByIDs[i].id))
-    //            _giveTaskBoolList[_giveByIDs[i].id] = true;
-    //    }
+		for (int i = 0; i < _giveByIDs.Count; i++)
+		{
+			_giveTaskBoolList.Add(false);
+		}
 
-    //    ActivateTakeAbilities();
-    //    ActivateGiveAbilities();
-    //}
+		for (int i = 0; i < _takeByIDs.Count; i++)
+		{
+			if (ListContainsIndex(_takeIDs, _takeByIDs[i].id))
+				_takeTaskBoolList[_takeByIDs[i].id] = true;
+		}
 
-    public void ActivateTakeAbilities()
-    {
-        int i = 0;
+		for (int i = 0; i < _giveByIDs.Count; i++)
+		{
+			if (ListContainsIndex(_giveIDs, _giveByIDs[i].id))
+				_giveTaskBoolList[_giveByIDs[i].id] = true;
+		}
 
-        if(_takeTaskBoolList[i]) // 0
-        {
-            TakeAbilities.instance.Ability0();
-        }
+		ActivateTakeAbilities();
+		ActivateGiveAbilities();
+	}
 
-        i++;
+	public void ActivateTakeAbilities()
+	{
+		int i = 0;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability1();
-        }
+		if (_takeTaskBoolList[i]) // 0
+		{
+			TakeAbilities.instance.Ability0();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability2();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability1();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability3();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability2();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability4();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability3();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i]) // 5
-        {
-            TakeAbilities.instance.Ability5();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability4();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability6();
-        }
+		if (_takeTaskBoolList[i]) // 5
+		{
+			TakeAbilities.instance.Ability5();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability7();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability6();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability8();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability7();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability9();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability8();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i]) // 10
-        {
-            TakeAbilities.instance.Ability10();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability9();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability11();
-        }
+		if (_takeTaskBoolList[i]) // 10
+		{
+			TakeAbilities.instance.Ability10();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability12();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability11();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability13();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability12();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability14();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability13();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i]) // 15
-        {
-            TakeAbilities.instance.Ability15();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability14();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability16();
-        }
+		if (_takeTaskBoolList[i]) // 15
+		{
+			TakeAbilities.instance.Ability15();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability17();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability16();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i])
-        {
-            TakeAbilities.instance.Ability18();
-        }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability17();
+		}
 
-        i++;
+		i++;
 
-        if (_takeTaskBoolList[i]) // 19
-        {
-            TakeAbilities.instance.Ability19();
-        }
-    }
+		if (_takeTaskBoolList[i])
+		{
+			TakeAbilities.instance.Ability18();
+		}
 
-    public void ActivateGiveAbilities()
-    {
-        int i = 0;
+		i++;
 
-        if(_giveTaskBoolList[i]) // 0
-        {
-            GiveAbilities.instance.Ability0();
-        }
+		if (_takeTaskBoolList[i]) // 19
+		{
+			TakeAbilities.instance.Ability19();
+		}
+	}
 
-        i++;
+	public void ActivateGiveAbilities()
+	{
+		int i = 0;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability1();
-        }
+		if (_giveTaskBoolList[i]) // 0
+		{
+			GiveAbilities.instance.Ability0();
+		}
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability2();
-        }
+		i++;
 
-        i++;
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability1();
+		}
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability3();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability2();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability4();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability3();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i]) // 5
-        {
-            GiveAbilities.instance.Ability5();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability4();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability6();
-        }
+		if (_giveTaskBoolList[i]) // 5
+		{
+			GiveAbilities.instance.Ability5();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability7();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability6();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability8();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability7();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability9();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability8();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i]) // 10
-        {
-            GiveAbilities.instance.Ability10();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability9();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability11();
-        }
+		if (_giveTaskBoolList[i]) // 10
+		{
+			GiveAbilities.instance.Ability10();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability12();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability11();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability13();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability12();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability14();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability13();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i]) // 15
-        {
-            GiveAbilities.instance.Ability15();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability14();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability16();
-        }
+		if (_giveTaskBoolList[i]) // 15
+		{
+			GiveAbilities.instance.Ability15();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability17();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability16();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability18();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability17();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability19();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability18();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i]) // 20
-        {
-            GiveAbilities.instance.Ability20();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability19();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability21();
-        }
+		if (_giveTaskBoolList[i]) // 20
+		{
+			GiveAbilities.instance.Ability20();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i])
-        {
-            GiveAbilities.instance.Ability22();
-        }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability21();
+		}
 
-        i++;
+		i++;
 
-        if (_giveTaskBoolList[i]) // 23
-        {
-            GiveAbilities.instance.Ability23();
-        }
-    }
+		if (_giveTaskBoolList[i])
+		{
+			GiveAbilities.instance.Ability22();
+		}
 
-    public void AddToTakeList(TaskAndGiveGenerator.TakeByID task)
-    {
-        _takeByIDs.Add(task);
-    }
+		i++;
 
-    public void AddToGiveList(TaskAndGiveGenerator.GiveByID task)
-    {
-        _giveByIDs.Add(task);
-    }
+		if (_giveTaskBoolList[i]) // 23
+		{
+			GiveAbilities.instance.Ability23();
+		}
+	}
 
-    private void ConvertToIDList(List<TaskAndGiveGenerator.TakeByID> takeByIDs)
-    {
-        _takeIDs = new();
+	public void AddToTakeList(TaskAndGiveGenerator.TakeByID task)
+	{
+		_takeByIDs.Add(task);
+	}
 
-        for (int i = 0; i < takeByIDs.Count; i++)
-        {
-            _takeIDs.Add(_takeByIDs[i].id);
-        }
-    }
+	public void AddToGiveList(TaskAndGiveGenerator.GiveByID task)
+	{
+		_giveByIDs.Add(task);
+	}
 
-    private void ConvertToIDList(List<TaskAndGiveGenerator.GiveByID> giveByIDs)
-    {
-        _giveIDs = new();
+	private void ConvertToIDList(List<TaskAndGiveGenerator.TakeByID> takeByIDs)
+	{
+		_takeIDs = new();
 
-        for (int i = 0; i < giveByIDs.Count; i++)
-        {
-            _giveIDs.Add(_giveByIDs[i].id);
-        }
-    }
+		for (int i = 0; i < takeByIDs.Count; i++)
+		{
+			_takeIDs.Add(_takeByIDs[i].id);
+		}
+	}
 
-    private bool ListContainsIndex(List<int> IDs, int index)
-    {
-        if(IDs.Contains(index))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+	private void ConvertToIDList(List<TaskAndGiveGenerator.GiveByID> giveByIDs)
+	{
+		_giveIDs = new();
+
+		for (int i = 0; i < giveByIDs.Count; i++)
+		{
+			_giveIDs.Add(_giveByIDs[i].id);
+		}
+	}
+
+	private bool ListContainsIndex(List<int> IDs, int index)
+	{
+		if (IDs.Contains(index))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
