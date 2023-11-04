f5afea59f31072d6f39b1d0b70c11d09803ca33a
LagsAlot <LucasPoethe@web.de>
2023-11-04 17:00:45 +0100

    Ability Setup.

---
+++ Assets/Scripts/Inventory/TakeAbilities.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeAbilities : Singleton<TakeAbilities>
+{
+    public void Ability0() // the first time you kill an enemy each stage you gain a top hat
+    {
+
+    }
+
+    public void Ability1() // at the beginning of each stage you gain a top hat
+    {
+
+    }
+
+    public void Ability2() // at the end of each stage you gain a top hat
+    {
+
+    }
+
+    public void Ability3() // the first time you only have 1 top hat left, you gain two top hats
+    {
+
+    }
+
+    public void Ability4() // the first damage you receive eachs tage is mitigated
+    {
+
+    }
+
+    public void Ability5() // you cannot get damaged for 2 seconds after getting damaged
+    {
+
+    }
+
+    public void Ability6() // you cannot get damaged for 2 seconds after killing an enemy
+    {
+
+    }
+
+    public void Ability7() // you cannot get damaged for the first 5 seconds of each stage
+    {
+
+    }
+
+    public void Ability8() // your movement speed is increased
+    {
+
+    }
+
+    public void Ability9() // your dash distance is increased
+    {
+
+    }
+
+    public void Ability10() // your dash coldown is decreased
+    {
+
+    }
+
+    public void Ability11() // the enemy movement speed is decreased
+    {
+
+    }
+
+    public void Ability12() // each stage you kill the first non-boss enemy you strike instantly
+    {
+
+    }
+
+    public void Ability13() // each stage you kill the second non-boss enemy you strike instantly
+    {
+
+    }
+
+    public void Ability14() // each stage you kill the fifth non-boss enemy you strike instantly
+    {
+
+    }
+
+    public void Ability15() // each stage you kill the tenth non-boss enemy you strike instantly
+    {
+
+    }
+
+    public void Ability16() // after you dash you take no damage for 0.5 esconds
+    {
+
+    }
+
+    public void Ability17() // after you dash you deal double damage for 2 seconds
+    {
+
+    }
+
+    public void Ability18() // after you dash 10 times in a stage you get a top hat
+    {
+
+    }
+
+    public void Ability19() // after you dash your movement speed is increased
+    {
+
+    }
+}
