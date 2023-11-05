722a11343ee99c24d8a0aa26a0b8dd87e4c5838b
19bobo94 <>
2023-11-05 14:22:26 +0100

    Navmesh Updated, Collectible Top Hats

--- Assets/Scripts/CollectibleTopHat.cs
+++ Assets/Scripts/CollectibleTopHat.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectibleTopHat : MonoBehaviour
 {
+    [SerializeField] int _amount = 1;
+    private AudioSource _source;
+    private bool _isCollected = false;
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _source = GetComponentInParent<AudioSource>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.GetComponent<TopHatCharacter>() != null && !_isCollected)
+        {
+            Health _health = other.GetComponent<Health>();
+
+            _health.IncreaseMaxHealth(_amount, false);
+            _health.Heal(_amount);
+
+            CollectTopHat();
+        }
+    }
+
+    private void CollectTopHat()
+    {
+        _isCollected = true;
+        _source.Play();
+        gameObject.SetActive(false);
     }
 }
